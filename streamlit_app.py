55bac90365ce5eba0825794ae3b6746e195e07a6
carlos-betancourt <carlos_betancourt@outlook.com>
2026-04-21 23:20:50 +0000

    Can't remember what was the last change I made to the repository to try to fix the errors. I think I added a .yml file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 # import snowflake.snowpark
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
+#from snowflake.snowpark.functions import col
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -16,7 +16,7 @@ st.write('The name on your Smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = cnx.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect(
     'Chhose up to 5 ingredients:'
