b3f71a0a0e3ae5769cd3bb7aee33f8f866a34435
carlos-betancourt <carlos_betancourt@outlook.com>
2026-04-21 21:27:38 +0000

    Trying to include the imoprt from snowflake.snowpark.functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 # import snowflake.snowpark
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-#from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col
 
 # Write directly to the app.
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
